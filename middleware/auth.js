2b525db8ed5638567c8074da9d54b200ab30440d
Chriss-ic <philchrisotieno@gmail.com>
2025-03-23 17:27:30 +0300

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,18 +1,16 @@
 import jwt from 'jsonwebtoken';
+import 'dotenv/config';
 
 export function auth(req, res, next) {
-  // Implement authentication logic 
-  const token = req.header('x-auth-token');
-  if (!token) return res.status(401).send({ success: false, message: 'Access denied. No token provided.' });
-  // Verify the token
-  try {
-    const decoded = jwt.verify(token,process.env.JWT_PRIVATE_KEY);
-    req.user = decoded;
-    next();
-  } catch (error) {
-    console.error(error);
-    res.status(400).send({ success: false, message: 'Invalid token' });
-  }
-}
+    const token = req.header('Authorization')?.replace('Bearer ', '');
+    if (!token) return res.status(401).json({ success: false, message: 'Access denied. No token provided.' });
 
-//this middlware allows only registered users to modify data
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_PRIVATE_KEY);
+        req.user = decoded;
+        next();
+    } catch (error) {
+        console.error(error);
+        res.status(400).json({ success: false, message: 'Invalid token.' });
+    }
+}
